122733a5e77e788da398ab6de9d6f6306424dda9
Thulani <104638674+ThusTech@users.noreply.github.com>
2022-05-19 21:21:18 +0200

    Code updated

--- task_0_5.py
+++ task_0_5.py
@@ -4,10 +4,3 @@ def area_of_triangle(first_side,second_side,third_side):
     area = (semi_perimeter*(semi_perimeter-first_side)*(semi_perimeter-second_side)*(semi_perimeter-third_side))**0.5
 
     return area
-
-
-first_side = float(input("Enter first side: "))
-second_side = float(input("Enter first side: "))
-third_side = float(input("Enter first side: "))
-
-print(f"Area of a triangle is {area_of_triangle(first_side,second_side,third_side):.2f} square centimeter")
