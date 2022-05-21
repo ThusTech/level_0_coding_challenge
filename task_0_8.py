31e6c0417b8f9e65bfaad59d9f2754c474a9f93d
Thulani <104638674+ThusTech@users.noreply.github.com>
2022-05-21 06:48:19 +0200

    Singular and plural forms error fixed

--- task_0_8.py
+++ task_0_8.py
@@ -2,7 +2,7 @@ def convert_to_time(number):
     hour = number // 60
     minute = number % 60
 
-    if hour > 1:
+    if hour > 1 or hour == 0:
         if minute > 1 or minute == 0:
             print(f"{hour} hours,{minute} minutes")
         elif minute == 1:
@@ -12,6 +12,4 @@ def convert_to_time(number):
             print(f"{hour} hour,{minute} minutes")
         elif minute == 1:
             print(f"{hour} hour,{minute} minute")
-    elif hour == 0:
-        print(f"{hour} hours, {minute} minutes")
-convert_to_time(61)
+convert_to_time(1)
